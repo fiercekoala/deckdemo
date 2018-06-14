7c72155c960f2f1b832bd510ccd5522361abb2b3
Harrison Barlow <harrison@dynamicgorilla.com>
2018-06-14 08:14:02 -0700

    Updates to Build and Shuffle Functions

--- deck.js
+++ deck.js
@@ -1,97 +1,75 @@
-function carddeck() {
+function cardDeck() {
 
-//variables to hold card info
-let deck = new Array();
-let suits = ["spades", "hearts", "diamonds", "clubs"];
-let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "D", "E"];
+  let deck = [];
+  let suits = ["spades", "hearts", "diamonds", "clubs"];
+  let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "D", "E"];
+   
+  function build() {
+    return values.map(function(cardValue) {
+      return suits.map(function(cardSuit) {
+        return {Value: cardValue, Suit: cardSuit};
+      });
+    }).reduce(function (a, b) { return a.concat(b) });
+  }
+  
+  function shuffleArray(arr){
+    return arr
+      .map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1])
+  }
 
-//build the deck of cards, for the object. 
-function build() {
-	for(var i = 0; i < suits.length; i++)
-		{
-			for(var x = 0; x < values.length; x++)
-			{
-				var card = {Value: values[x], Suit: suits[i]};
-				deck.push(card);
-				//console.log(values[x]);
-			}
-		}
-	deckholder.remainingcards();
+  //Object to be returned to frontend
+  let deck_holder = {
+  
+    //Shuffle the current deck.
+    shuffle:() => {
+      (Array.isArray(deck) && deck.length === 0) ? (
+        document.getElementById("message").innerHTML = "Deck Shuffled", deck = build()) 
+        : document.getElementById("message").innerHTML = "Remaining Deck Shuffled";
+      deck = shuffleArray(deck);
+    },
+  
+    //Simulate the Draw Action, force a shuffle if the deck has not been shuffled
+    draw:() => {
+      if(Array.isArray(deck) && deck.length === 0){
+        deckholder.shuffle();
+        document.getElementById("message").innerHTML = "Deck Shuffled";
+        document.getElementById("displaydraw").innerHTML = "&#x1F0A0;";
+        document.getElementById("remainingcards").innerHTML = deck.length;
+        return;
+      }
+      //clear the card display
+      document.getElementById("message").innerHTML = "";
+      document.getElementById("displaydraw").innerHTML = "";
+      var card = document.createElement("div");
+      card.className = deck[0].Suit;
+      switch(deck[0].Suit) {
+           case "spades":
+             card.innerHTML = "&#x1F0A" + deck[0].Value + ";";
+             break;
+           case "hearts":
+             card.innerHTML = "&#x1F0B" + deck[0].Value + ";";
+             break;
+           case "diamonds":
+             card.innerHTML = "&#x1F0C" + deck[0].Value + ";";
+             break;
+           case "clubs":
+             card.innerHTML = "&#x1F0D" + deck[0].Value + ";";
+             break;
+           default:
+              card.innerHTML = "No Card Found";
+      }
+      deck.shift();
+      document.getElementById("displaydraw").appendChild(card);
+      document.getElementById("remainingcards").innerHTML = deck.length;
+    },
+
+    // Testing function to show cards in console.
+    showDeck:() => { console.log(deck); },
+  };
+
+  return deck_holder;
 }
 
-//function to check if the current deck is empty, if
-function emptydeck(){
-	if(!deck.length){
-			build();
-			return true;
-		}
-	return false;
-}
-
-//Object to be returned to frontend
-let deckholder = {
-
-	//Shuffle the current deck. This will shuffle whatever cards remain in the deck. 
-	shuffle:() =>
-	{
-		emptydeck() ? document.getElementById("message").innerHTML = "Deck Shuffled" : document.getElementById("message").innerHTML = "Remaining Deck Shuffled";
-		for (var i = (deck.length-1); i > 0; i--)
-		{
-			var randomNumber = Math.floor((Math.random() * i));
-			var temp = deck[i];
-
-			deck[i] = deck[randomNumber];
-			deck[randomNumber] = temp;
-		}
-	},
-
-	//Simulate the Draw Action, remove the first element from the top of the array, force a shuffle if the deck has not been shuffled
-	draw:() =>
-	{
-		if(emptydeck()){
-			deckholder.shuffle();
-			document.getElementById("message").innerHTML = "Deck Shuffled";
-			document.getElementById("displaydraw").innerHTML = "&#x1F0A0;";
-			return;
-		}
-
-		//clear the card display
-		document.getElementById("message").innerHTML = "";
-		document.getElementById("displaydraw").innerHTML = "";
-		var card = document.createElement("div");
-		card.className = deck[0].Suit;
-		switch(deck[0].Suit) {
-		    case "spades":
-		        card.innerHTML = "&#x1F0A" + deck[0].Value + ";";
-		        break;
-		    case "hearts":
-		        card.innerHTML = "&#x1F0B" + deck[0].Value + ";";
-		        break;
-		    case "diamonds":
-		        card.innerHTML = "&#x1F0C" + deck[0].Value + ";";
-		        break;
-		    case "clubs":
-		        card.innerHTML = "&#x1F0D" + deck[0].Value + ";";
-		        break;
-		    default:
-		         card.innerHTML = "No Card Found";
-		}
-		deck.shift();
-
-		document.getElementById("displaydraw").appendChild(card);
-		deckholder.remainingcards();
-	},
-
-	remainingcards:() =>
-	{
-		document.getElementById("remainingcards").innerHTML = deck.length;
-	},
-
-	// Testing function to show cards in console.
-	//showdeck:() => { console.log(deck); },
-};
-
-	return deckholder;
-}
-
-const deck1 = carddeck();
+const initial_deck = cardDeck();  
